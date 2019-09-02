3e81fa9d2b7a3fc97d6ab7a603d09ac521fdd99d
jehuddleston <jehuddles17@gmail.com>
2019-09-02 17:02:10 -0400

    Added REPL to main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
-fn main() {
-    println!("Hello, world!");
+use std::io::{self, BufRead};
+
+fn main(){
+    let mut check = true;
+
+    while check{
+        let mut buffer = String::new();
+        let stdin = io::stdin();
+        stdin.lock().read_line(&mut buffer).unwrap();
+        println!("Read in {}", buffer);
+        if buffer == "quit\n"
+        {check = false;}
+    }
 }
+
